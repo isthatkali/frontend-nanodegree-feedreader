276bcc183b292baf7400fae3f98327124eb0cd25
Kali <isthatkali@gmail.com>
2018-10-10 04:39:17 -0700

    added test to check for at least one entry after feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -73,13 +73,22 @@ $(function() {
     });
       
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {        
         /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        * function is called and completes its work, there is at least
+        * a single .entry element within the .feed container.
+        * Remember, loadFeed() is asynchronous so this test will require
+        * the use of Jasmine's beforeEach and asynchronous done() function.
+        */
+       beforeEach(function(done) {
+            loadFeed(0, done); // Got stuck here and had to reference https://matthewcranford.com/feed-reader-walkthrough-part-4-async-tests/
+       });
+
+       it('has at least one entry after feed is loaded', function() {
+            expect(($('.feed').length)).toBeGreaterThan(0);
+       });
+
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
