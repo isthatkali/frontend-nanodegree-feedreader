5cfa57a54cfa2be55691ba8d4f50912f85abfbad
Kali <isthatkali@gmail.com>
2018-10-11 14:51:13 -0700

    fixed url and name checks

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,23 +26,15 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
         it('has url', function() {
             for (feed of allFeeds) {
-                expect(feed.url).toContain('http://');
+                expect(feed.url.lenth).not.toBe(0);
             }
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
         it('has name', function() {
             for (feed of allFeeds) {
-                expect(feed.name).not.toBe('');
+                expect(feed.name.length).not.toBe(0);
             }
         });
     });
